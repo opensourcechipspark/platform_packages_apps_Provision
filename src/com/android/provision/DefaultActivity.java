2d768874217407acfbc9991426350bcd388245d9
opensourcechipspark <opensource@chipspark.com>
2015-05-29 10:40:11 +0800

    popmetal init commit

--- src/com/android/provision/DefaultActivity.java
+++ src/com/android/provision/DefaultActivity.java
@@ -21,7 +21,12 @@ import android.content.ComponentName;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.provider.Settings;
-
+import android.content.pm.ActivityInfo;
+import android.content.pm.ApplicationInfo;
+import android.content.Context;
+import android.os.SystemProperties;
+import java.util.List;
+import android.content.pm.PackageInfo;
 /**
  * Application that sets the provisioned bit, like SetupWizard does.
  */
@@ -37,6 +42,7 @@ public class DefaultActivity extends Activity {
 
         // remove this activity from the package manager.
         PackageManager pm = getPackageManager();
+		disable_APP(getApplicationContext());
         ComponentName name = new ComponentName(this, DefaultActivity.class);
         pm.setComponentEnabledSetting(name, PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
                 PackageManager.DONT_KILL_APP);
@@ -44,5 +50,54 @@ public class DefaultActivity extends Activity {
         // terminate the activity.
         finish();
     }
+
+	 private static final String packageNameDialer[]   = {
+			 "com.android.dialer",
+	 };
+	 private static final String packageNameMMS[]    = {
+			 "com.android.mms"
+	 };
+
+	 private void disable_APP(Context mcontext){
+		 PackageManager pm = getPackageManager();
+		 setApplicationEnabled(
+				 packageNameMMS,
+				  SystemProperties.getBoolean("ro.sms.capable",false),
+				 //false,
+				 pm);
+		 setApplicationEnabled(
+				 packageNameDialer,
+				 SystemProperties.getBoolean("ro.voice.capable",false),
+				// false,
+				 pm);
+	 }
+
+
+
+	 private void setApplicationEnabled(String packageName[], boolean enable,
+              PackageManager pm) {
+		          for (int i = 0, j = packageName.length; i < j; i++) {
+				  if (isPackageInstalled(packageName[i], pm)) {
+					  pm.setApplicationEnabledSetting(
+							  packageName[i],
+							  enable ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+							  : PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+							  PackageManager.DONT_KILL_APP);
+				  }
+			  }
+	 }
+
+	 private boolean isPackageInstalled(String packageName, PackageManager pm) {
+		         List<PackageInfo> installedList = pm
+		                 .getInstalledPackages(PackageManager.GET_UNINSTALLED_PACKAGES);
+		         int installedListSize = installedList.size();
+		        for (int i = 0; i < installedListSize; i++) {
+				             PackageInfo tmp = installedList.get(i);
+				             if (packageName.equalsIgnoreCase(tmp.packageName)) {
+						                 return true;
+						             }
+			}
+			return false;
+	 }
 }
 
